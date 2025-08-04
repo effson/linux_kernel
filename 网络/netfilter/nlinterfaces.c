aaebce973146d8f49d965d4f44cd4e6fffe46b46
effson <113086278+effson@users.noreply.github.com>
2025-08-05 01:29:58 +0800

    Update nlinterfaces.c

--- 网络/netfilter/nlinterfaces.c
+++ 网络/netfilter/nlinterfaces.c
@@ -1,3 +1,6 @@
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,13 +8,16 @@
 #include <sys/socket.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
+#include <net/if.h>
+#include <arpa/inet.h>
 
-#define BUFSIZE 1024
+#define BUFSIZE 8192
 
 struct nl_req_s {
   struct nlmsghdr hdr;
   struct rtgenmsg gen;
 };
+
 /* 
 +----------------+
 | nlmsghdr       |  (通用 Netlink 消息头部)
@@ -21,20 +27,19 @@ struct nl_req_s {
 | Netlink Attributes | (可选的 TLV 格式属性，提供额外详细信息)
 +----------------+
 */
-void rtnetfilter_print_link(struct nlmsghdr *h) 
+
+void rtnetfilter_print_link(struct nlmsghdr *nlh) 
 {
-/* struct ifinfomsg 是 Linux 内核和用户空间之间
+  /* struct ifinfomsg 是 Linux 内核和用户空间之间
   通过 Netlink 协议交换网络接口信息时使用的数据结构，
   主要用于描述 网络设备（network interface）状态*/
-  struct ifinfomsg *iface;
+  struct ifinfomsg *iface = NLMSG_DATA(nlh);
   /*Netlink Route（rtnetlink）消息中一个“Type-Length-Value (TLV)” 
   格式的单个属性头，用于封装附加字段，比如接口名、MAC 地址、MTU 等*/
-  struct rtattr *attr; 
-  int len = 0;
-  iface = NLMSG_DATA(h);
-  len = RTM_PAYLOAD(h);
+  struct rtattr *attr = IFLA_RTA(iface); 
+  int len = IFLA_PAYLOAD(nlh);
 
-  /* [ Netlink header ]
+    /* [ Netlink header ]
      [ struct ifinfomsg ]  ← 固定字段
      [ struct rtattr ]     ← attribute 1
      [ struct rtattr ]     ← attribute 2 
@@ -47,12 +52,13 @@ void rtnetfilter_print_link(struct nlmsghdr *h)
           break;
         case IFLA_ADDRESS: // 网络接口的硬件地址 MAC
           const unsigned char *mac_addr = (unsigned char *)RTA_DATA(attr);
+                // 确保地址长度是有效的，例如 ETH_ALEN (6字节)
           if (RTA_PAYLOAD(attr) >= 6) { // 检查属性的有效载荷长度是否至少为6字节
-            printf("  MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+            printf("MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
                     mac_addr[0], mac_addr[1], mac_addr[2],
                     mac_addr[3], mac_addr[4], mac_addr[5]);
           } else {
-            printf("  MAC Address (invalid length): ");
+            printf("MAC Address (invalid length): ");
             // 可以选择打印原始字节或进行其他错误处理
             for (int i = 0; i < RTA_PAYLOAD(attr); i++) {
               printf("%02x%s", mac_addr[i], (i == RTA_PAYLOAD(attr) - 1) ? "" : ":");
@@ -60,37 +66,50 @@ void rtnetfilter_print_link(struct nlmsghdr *h)
             printf("\n");
           }
           break;
+        case IFA_LOCAL:
+          // struct in_addr *in = (struct in_addr *)RTA_DATA(attr);
+          // char ip[INET_ADDRSTRLEN] = {0};
+          // inet_ntop(AF_INET, in, ip, sizeof(ip));
+          // printf("  IP Address: %s\n", ip);
+          int ip = *(int *)RTA_DATA(attr);
+          unsigned char bytes[4];
+          bytes[0] = ip & 0xFF;
+          bytes[1] = (ip >> 8) & 0xFF;
+          bytes[2] = (ip >> 16) & 0xFF;
+          bytes[3] = (ip >> 24) & 0xFF;
+          printf("IP Address : %d.%d.%d.%d\n",bytes[0],bytes[1],bytes[2],bytes[3]);
+          break;
         default:
           break;
     }
   }
 }
 
-int main (int argc, char *argv[]) {
+int main (int argc, char *argv[]) 
+{
+  char buf[BUFSIZE];
+  int sock = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+  if (sock < 0) {
+    perror("netlink socket fail.\n");
+    exit(1);
+  }
+
   struct sockaddr_nl nlsock;
   struct msghdr msg;
   struct iovec io;
 
   struct nl_req_s req;
-  int s = 0, end = 0, len = 0;
-  char buf[BUFSIZE];
 
   memset(&nlsock, 0, sizeof(nlsock));
   nlsock.nl_family = AF_NETLINK;
-  nlsock.nl_groups = 0;
-  s = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-  if (s < 0) {
-    perror("netlink socket fail.\n");
-    exit(1);
-  }
 
   memset(&req, 0, sizeof(req));
   req.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtgenmsg));
   req.hdr.nlmsg_type = RTM_GETLINK; // 请求内核返回当前系统上所有网络接口（或指定接口）的详细信息
+  //RTM_NEWLINK、RTM_DELLINK、RTM_GETLINK
   req.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP; //指示内核返回指定类型的所有现有对象
   req.hdr.nlmsg_seq = 1; // 序列号，用于排列消息处理
   req.hdr.nlmsg_pid = getpid();
-
   req.gen.rtgen_family = AF_INET; // ipv4
 
   memset(&io, 0, sizeof(io));
@@ -103,19 +122,19 @@ int main (int argc, char *argv[]) {
   msg.msg_name = &nlsock;
   msg.msg_namelen = sizeof(nlsock);
 
-  s = sendmsg(s, &msg, 0);
-  if (s < 0) {
+  if (sendmsg(sock, &msg, 0) < 0) {
     perror("sendmsg\n");
     exit(1);
   }
 
-  while (!end) 
+  int done = 0;
+  while (!done) 
   {
     memset(buf, 0, BUFSIZE);
     msg.msg_iov->iov_base = buf;
     msg.msg_iov->iov_len = BUFSIZE;
 
-    len = recvmsg(s, &msg, 0);
+    ssize_t len = recvmsg(sock, &msg, 0);
     if (len < 0) 
     {
       perror("recvmsg");
@@ -125,11 +144,19 @@ int main (int argc, char *argv[]) {
       switch (msg_ptr->nlmsg_type)
       {
         case NLMSG_DONE:
-          end ++;
+          done = 1;
           break;
         case RTM_NEWLINK:
           rtnetfilter_print_link(msg_ptr);
           break;
+        case RTM_NEWADDR:
+          rtnetfilter_print_link(msg_ptr);
+          break;
+        case NLMSG_ERROR:
+          struct nlmsgerr *err = (struct nlmsgerr *)NLMSG_DATA(msg_ptr);
+          fprintf(stderr, "Received NLMSG_ERROR: code = %d\n", err->error);
+          done = 1;
+          break;  
         default:
           printf("ignore msg: [type] %d, len = %d\n", msg_ptr->nlmsg_type, msg_ptr->nlmsg_len);
           break;
